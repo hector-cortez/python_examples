423acb51cb9062aaca767ee3754fadea5a3cd2b6
hcortez <hector.cortez.ch@gmail.com>
2016-05-21 00:07:48 -0400

    second commit
    Dada la base B y la altura H de un triángulo rectángulo, hallar su
    área.

---
+++ ej002.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+'''
+@author: Hector Cortez
+Dada la base B y la altura H de un triángulo rectángulo, hallar su área. 
+Desplegar los lados y el area obtenida.
+'''
+import math
+b = int(input("Introduzca la base: "))
+h = int(input("Introduzca la altura: "))
+
+area = (b * h) / 2
+print "base: ", b
+print "altura:", h
+print "area:", area
