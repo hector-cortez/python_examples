88d5130fd4af4f6509dae2fc5c3bf6f409040673
hcortez <hector.cortez.ch@gmail.com>
2016-05-20 23:54:03 -0400

    first commit
    A basic example

---
+++ ej001.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Leer un numero X, hallar su cuadrado, cubo, raiz cuadrada, raiz cubica. desplegar los resultados.
+import math
+x = int(input("Digite un numero: "))
+cuadrado = x * x
+cubo = x * x * x
+raiz = math.sqrt(x)
+print cuadrado
+print cubo
+print raiz
+
