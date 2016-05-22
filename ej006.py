f8204723846365a7f74a519e5caf3168f142b971
hcortez <hector.cortez.ch@gmail.com>
2016-05-22 22:35:33 +0000

    Adjustment utf-8

--- ej006.py
+++ ej006.py
@@ -2,9 +2,9 @@
 '''
 @author: Hector Cortez
 Dado un número A determinar si es par o impar, utilizando el modulo de una division.
-desplegar el nÃmero y el mensaje. 
+desplegar el número y el mensaje. 
 '''
-n = int(input("Introduzca un nÃºmero: "))
+n = int(input("Introduzca un número: "))
 if (n % 2) == 0:
     print n, " es par"
 else:
