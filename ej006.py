9561b95ea44a05d0e5368d7ee7261f979ed625fb
hcortez <hector.cortez.ch@gmail.com>
2016-05-22 22:03:43 +0000

    Determinar si un número es par o impar

---
+++ ej006.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+'''
+@author: Hector Cortez
+Dado un númeroA determinar si es par o impar, utilizando el modulo de una division.
+desplegar el n�mero y el mensaje. 
+'''
+n = int(input("Introduzca un número: "))
+if (n % 2) == 0:
+    print n, " es par"
+else:
+    print n, " es impar"
+
